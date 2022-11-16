34f7600c669ce95a18434a0f9b9f844340b18493
Carlos Lopez <rlcarlosvladimir@gmail.com>
2022-11-16 15:48:47 -0500

    Fixed kg and g problem, and added mg and oz

--- unitconverter.py
+++ unitconverter.py
@@ -15,24 +15,21 @@
 #
 #
 #
-import sys
-s = input("What are we converting? ")
-
+s = input("What are wwe converting? ")
 class Unit:
   def __init__(self, unitname, convertsto, conversionfactor):
     self.unitname = unitname
     self.convertsto = convertsto
     self.conversionfactor = conversionfactor
-
 def convert(s):
-   [from_, to_] = s.casefold().split(' to ')
-
-   for i in [Unit('kg', ['lbs', 'g', 'mg', 'oz'], [2.2046244202, 1000, 1000000, 35.273990723]),
-            Unit('lbs', ['oz', 'kg', 'g', 'mg'], [16, 0.453592, 452.592, 453592]),
-            Unit('g', ['kg', 'mg', 'lbs', 'oz'], [0.001, 1000, 0.0022046244, 0.0352739907])]:
+  [from_, to_] = s.split(' to ')
+  for i in [Unit('kg', ['lbs', 'gm', 'mg', 'oz'], [2.2046244202, 1000, 1000000, 35.273990723]),
+            Unit('lbs', ['oz', 'kg', 'gm', 'mg'], [16, 0.453592, 452.592, 453592]),
+            Unit('gm', ['kg', 'mg', 'lbs', 'oz'], [0.001, 1000, 0.0022046244, 0.0352739907]),
+            Unit('mg', ['kg', 'gm', 'lbs', 'oz'], [0.000001, 0.001, 0.0000022046, 0.000035274]),
+            Unit('oz', ['kg', 'gm', 'mg', 'lbs'], [0.0283495, 28.3495, 28349.5, 0.0625])]:
       if from_.endswith(i.unitname):
           val = float(from_.removesuffix(i.unitname))
           ind = i.convertsto.index(to_)
           print(val * i.conversionfactor[ind])
-
 convert(s)
